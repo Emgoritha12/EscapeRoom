a92cc93b5c9b1d6f4b5240a44017716c231b7221
Emgoritha12 <97557385+Emgoritha12@users.noreply.github.com>
2022-04-08 23:50:52 -0400

    Add files via upload

--- script3.js
+++ script3.js
@@ -1,9 +1,10 @@
-function colorAnswer() {
-   var password = document.getElementById("passwordBox2");
+function Riddle() {
+   var password = document.getElementById("Riddle1");
   /*add .toUpperCase() String function so that even if the user types in the password in lower-case, it will check against uppercase password text*/
    var passwordText = password.value.toUpperCase();
-   if(passwordText == "20°40'59.0"N 88°34'7"W") {
-    return true;
+   if(passwordText == "CRYSTAL SKULL") {
+    alert("You have opened the first door"); 
+   return true;
    }
    alert("That is incorrect, try again!");
    return false;
